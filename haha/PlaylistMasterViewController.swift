7f691611ffcd3331a5019690997b4c921b324d65
David Elliott <david@gophilosophie.com>
2015-06-19 21:41:09 -0700

    adds playlist image views

--- haha/PlaylistMasterViewController.swift
+++ haha/PlaylistMasterViewController.swift
@@ -21,7 +21,15 @@ class PlaylistMasterViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    playlistArray.append(playlistImageView0)
+    playlistArray += [
+      playlistImageView0,
+      playlistImageView1,
+      playlistImageView2,
+      playlistImageView3,
+      playlistImageView4,
+      playlistImageView5
+    ]
+
     for index in 0..<playlistArray.count {
       let playlist = Playlist(index: index)
       let playlistImageView = playlistArray[index]
